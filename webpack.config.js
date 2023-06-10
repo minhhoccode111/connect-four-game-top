f81cd2dee70d9434d413920272f0b04e61fa90b0
minhhoccode111 <minhhoccode111@gmail.com>
2023-06-10 13:54:01 +0700

    commit before switch branch

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
   },
 };
